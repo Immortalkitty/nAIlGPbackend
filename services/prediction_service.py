f5b9bc1d28cd8d94f06554bf168165a50d5f8ea5
kasia <katarzyna.szczerba1112@gmail.com>
2024-11-10 21:29:45 +0100

    Clean

--- services/prediction_service.py
+++ services/prediction_service.py
@@ -17,7 +17,7 @@ class PredictionService:
 
     def load_model(self):
         try:
-            print(f"Loading PyTorch model from {self.model_path}...")  # Using standard print or logging
+            print(f"Loading PyTorch model from {self.model_path}...")
             model_initializer = ModelInitializer(self.device, model_name='ResNet50', weights_suffix='DEFAULT')
             model = model_initializer.initialize_model()
             model.load_state_dict(torch.load(self.model_path, map_location=self.device))
@@ -25,7 +25,7 @@ class PredictionService:
             print("Model loaded successfully.")
             return model
         except Exception as e:
-            print(f"Error loading PyTorch model: {e}")  # Handle error logging without current_app context
+            print(f"Error loading PyTorch model: {e}")
             raise e
 
     def preprocess_image(self, image_path):
@@ -38,8 +38,7 @@ class PredictionService:
             current_app.logger.info(f"Loading and preprocessing image from {image_path}...")
 
         preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize(299),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
